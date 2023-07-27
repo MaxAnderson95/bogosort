0d4983584c905fe6017f8332395f012f6b7d4172
Max Anderson <cctpmax@gmail.com>
2023-07-27 15:06:45 -0400

    bogosort func doesn't return anything

--- example.py
+++ example.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     # Generate a random list of random length between 5 and 10
     list = random.sample(range(1, 100), random.choice(range(5, 10)))
     print("Random list:", list)
-    sorted_list = bogosort(list)
+    bogosort(list)
